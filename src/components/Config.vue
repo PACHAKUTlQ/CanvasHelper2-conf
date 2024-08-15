e82efbfc75bac48ced2c76950d73c064702d3752
Pachakutiq <null@gmail.com>
2024-08-15 18:33:44 +0800

    feat: Only update (put) changed attributes in config

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -12,17 +12,10 @@
       <p>
         Access Token of your canvas account. (<a
           href="https://community.canvaslms.com/t5/Admin-Guide/How-do-I-manage-API-access-tokens-as-an-admin/ta-p/89"
-          target="_blank"
-          >Tutorial for getting an access token</a
-        >)
+          target="_blank">Tutorial for getting an access token</a>)
       </p>
-      <a-input
-        v-model:value="token"
-        placeholder="Access Token"
-        type="password"
-      />
-      <a-typography-title :level="3"
-        >Semester
+      <a-input v-model:value="token" placeholder="Access Token" type="password" />
+      <a-typography-title :level="3">Semester
         <a-badge count="v 2.0" :number-style="{ backgroundColor: '#52c41a' }" />
       </a-typography-title>
       <p>The date when the semester begins.</p>
@@ -36,21 +29,16 @@
       </p>
       <a-input v-model:value="title" placeholder="Title" />
 
-      <a-typography-title :level="3"
-        >Time format
-        <a-badge count="v 1.5" :number-style="{ backgroundColor: '#52c41a' }"
-      /></a-typography-title>
+      <a-typography-title :level="3">Time format
+        <a-badge count="v 1.5" :number-style="{ backgroundColor: '#52c41a' }" /></a-typography-title>
       <p>The time format. Default value is relative.</p>
       <ul>
         <li>Relative: Use relative date.</li>
         <li>Origin: Use unformatted date.</li>
         <li>
           strftime: Use
-          <a
-            target="_blank"
-            href="https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes"
-            >strftime</a
-          >
+          <a target="_blank"
+            href="https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes">strftime</a>
           date.
         </li>
       </ul>
@@ -59,60 +47,33 @@
         <a-radio-button value="origin">Origin</a-radio-button>
         <a-radio-button value="strftime">strftime</a-radio-button>
       </a-radio-group>
-      <a-input
-        v-model:value="timeFormat"
-        placeholder="strftime string"
-        v-if="time_format_mode == 'strftime'"
-      />
+      <a-input v-model:value="timeFormat" placeholder="strftime string" v-if="time_format_mode == 'strftime'" />
 
       <a-typography-title :level="3">Background</a-typography-title>
       <p>The background image/video of your canvas dashboard.</p>
       <a-radio-group v-model:value="background_mode" style="width: 100%">
         <a-radio-button value="image">Image</a-radio-button>
         <a-radio-button value="video">Video</a-radio-button>
-        <a-select
-          v-model:value="background_url"
-          size="middle"
-          style="width: 200px"
-          :options="background_file_name"
-        ></a-select>
+        <a-select v-model:value="background_url" size="middle" style="width: 200px"
+          :options="background_file_name"></a-select>
       </a-radio-group>
-      <a-upload
-        v-model:file-list="background_filelist"
-        list-type="picture"
-        :action="base_url + '/file/upload'"
-        :headers="headers"
-        @change="onChangeFilelist"
-      >
+      <a-upload v-model:file-list="background_filelist" list-type="picture" :action="base_url + '/file/upload'"
+        :headers="headers" @change="onChangeFilelist">
         <a-button>
           <upload-outlined></upload-outlined>
           Upload File
         </a-button>
       </a-upload>
       <a-divider orientation="center">Experimental</a-divider>
-      <a-button type="primary" @click="force_reload" :loading="reload_loading"
-        >Force Reload From Disk</a-button
-      >
-      <a-button type="primary" @click="verify" :loading="verify_loading"
-        >Verify Configuration</a-button
-      >
-      <a-button
-        type="primary"
-        danger
-        @click="remove_userdata"
-        :loading="remove_userdata_loading"
-        >Remove all user data</a-button
-      >
-      <a-alert
-        message="Warning"
+      <a-button type="primary" @click="force_reload" :loading="reload_loading">Force Reload From Disk</a-button>
+      <a-button type="primary" @click="verify" :loading="verify_loading">Verify Configuration</a-button>
+      <a-button type="primary" danger @click="remove_userdata" :loading="remove_userdata_loading">Remove all user
+        data</a-button>
+      <a-alert message="Warning"
         description="This part is still under heavy development. Feel free to contribute if you are interested!"
-        type="warning"
-        show-icon
-      />
+        type="warning" show-icon />
       <a-row justify="center" style="margin-top: 30px">
-        <a-button type="primary" @click="submit" :loading="submit_loading"
-          >Save</a-button
-        >
+        <a-button type="primary" @click="submit" :loading="submit_loading">Save</a-button>
       </a-row>
     </a-space>
   </Page>
@@ -123,10 +84,8 @@ import Page from "./PageSlot.vue";
 import dayjs from "dayjs";
 import type { UploadChangeParam } from "ant-design-vue";
 import { del, get, Base_url, put } from "../tools/requests";
-
 import { message } from "ant-design-vue";
 import { defineComponent } from "vue";
-
 import { getAccessTokenHeaders } from "../../localStorages";
 
 interface UploadProps {
@@ -154,25 +113,27 @@ export default defineComponent({
         time_format_mode: string;
         background_filelist: UploadProps[];
         base_url: string;
+        originalConfig: Record<string, any>;
       }
-    >{
-      title: "",
-      loading: true,
-      url: "",
-      token: "",
-      semester: dayjs(),
-      timeFormat: "",
-      background_mode: "",
-      background_url: "",
-      verify_loading: false,
-      reload_loading: false,
-      submit_loading: false,
-      remove_userdata_loading: false,
-      has_err: false,
-      time_format_mode: "relative",
-      background_filelist: [],
-      base_url: Base_url,
-    };
+      >{
+        title: "",
+        loading: true,
+        url: "",
+        token: "",
+        semester: dayjs(),
+        timeFormat: "",
+        background_mode: "",
+        background_url: "",
+        verify_loading: false,
+        reload_loading: false,
+        submit_loading: false,
+        remove_userdata_loading: false,
+        has_err: false,
+        time_format_mode: "relative",
+        background_filelist: [],
+        base_url: Base_url,
+        originalConfig: {} // To store original config values
+      };
   },
   methods: {
     async onChangeFilelist(change: UploadChangeParam) {
@@ -185,7 +146,6 @@ export default defineComponent({
         return;
       }
       if (change.file.status == "removed") {
-        // Remove file
         const res = await del(`/file?name=${change.file.name}`, {
           headers: getAccessTokenHeaders(),
         });
@@ -198,9 +158,8 @@ export default defineComponent({
       }
     },
     async remove_userdata() {
-      // Remove position & checks
+      // Remove checks
       this.remove_userdata_loading = true;
-      // await this.remove_attribute('position');
       await this.remove_attribute("checks");
       this.remove_userdata_loading = false;
       message.success("Deleted user data");
@@ -245,10 +204,8 @@ export default defineComponent({
       }
     },
     async reload() {
-      // Reload configuration
       const res = await get("/config", { headers: getAccessTokenHeaders() });
       if (res && res.status === 200) {
-        // Got configuration
         const conf = res.data;
         this.title = conf.title;
         this.url = conf.url;
@@ -272,6 +229,9 @@ export default defineComponent({
         this.background_url = conf.background_image
           ? conf.background_image
           : conf.video;
+
+        // Store the original configuration values
+        this.originalConfig = { ...conf };
       } else {
         message.error("Getting configuration failed");
       }
@@ -280,7 +240,6 @@ export default defineComponent({
       this.loading = false;
     },
     async verify() {
-      // Verify the configuration
       this.verify_loading = true;
       const res = await get("/config/verify", {
         headers: getAccessTokenHeaders(),
@@ -295,7 +254,6 @@ export default defineComponent({
       this.verify_loading = false;
     },
     async force_reload() {
-      // Force Reload
       this.reload_loading = true;
       const res = await get("/config/refresh", {
         headers: getAccessTokenHeaders(),
@@ -309,27 +267,35 @@ export default defineComponent({
       this.reload_loading = false;
     },
     async submit() {
-      // Submit form
       this.submit_loading = true;
-      if (this.semester) {
-        const sem_f = this.semester.format("YYYY-MM-DD");
-        await this.set_attribute("semester_begin", sem_f);
+      const changes = [];
+
+      if (this.semester.format("YYYY-MM-DD") !== this.originalConfig.semester_begin) {
+        changes.push(this.set_attribute("semester_begin", this.semester.format("YYYY-MM-DD")));
       }
-      await this.set_attribute("title", this.title);
-      await this.set_attribute("url", this.url);
-      await this.set_attribute("bid", this.token);
-      if (this.time_format_mode != "strftime") {
-        await this.set_attribute("timeformat", this.time_format_mode);
-      } else {
-        await this.set_attribute("timeformat", this.timeFormat);
+      if (this.title !== this.originalConfig.title) {
+        changes.push(this.set_attribute("title", this.title));
       }
-      if (this.background_mode == "image") {
-        await this.set_attribute("background_image", this.background_url);
-        await this.set_attribute("video", "");
-      } else if (this.background_mode == "video") {
-        await this.set_attribute("video", this.background_url);
-        await this.set_attribute("background_image", "");
+      if (this.url !== this.originalConfig.url) {
+        changes.push(this.set_attribute("url", this.url));
       }
+      if (this.token !== this.originalConfig.bid) {
+        changes.push(this.set_attribute("bid", this.token));
+      }
+      if (this.time_format_mode !== this.originalConfig.time_format) {
+        changes.push(this.set_attribute("timeformat", this.time_format_mode));
+      } else if (this.time_format_mode === "strftime" && this.timeFormat !== this.originalConfig.timeformat) {
+        changes.push(this.set_attribute("timeformat", this.timeFormat));
+      }
+      if (this.background_mode === "image" && this.background_url !== this.originalConfig.background_image) {
+        changes.push(this.set_attribute("background_image", this.background_url));
+        changes.push(this.set_attribute("video", ""));
+      } else if (this.background_mode === "video" && this.background_url !== this.originalConfig.video) {
+        changes.push(this.set_attribute("video", this.background_url));
+        changes.push(this.set_attribute("background_image", ""));
+      }
+
+      await Promise.all(changes);
 
       if (!this.has_err) {
         message.success("Configuration saved");
@@ -343,7 +309,6 @@ export default defineComponent({
       return getAccessTokenHeaders();
     },
     background_file_name() {
-      // console.log(this.background_filelist)
       let res = [];
       for (const item of this.background_filelist) {
         res.push({ value: item.name });
